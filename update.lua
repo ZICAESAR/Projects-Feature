691bd6b941ccaec44dab4c6310475019439a2c33
ZISARHUB <95739957+ZISARHUB@users.noreply.github.com>
2021-12-20 21:14:05 +0700

    Create update.lua

---
+++ update.lua
@@ -0,0 +1,17 @@
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/EPhsQWK9",true))()
+
+wait(3)
+
+local Window = library:CreateWindow("IMAINHUB")
+
+Window:CreateButton("BF",30,function() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZISARHUB/EEZZ/main/IMainHub.lua"))()
+end)
+
+Window:CreateButton("KL",30,function() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZISARHUB/EEZZ/main/IMainHub.lua"))()
+end)
+
+Window:CreateButton("GPO",30,function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZISARHUB/EEZZ/main/IMainHub.lua"))()
+end)
