3116909f4e0f89a501e5acab170d4da533f53164
ZISARHUB <95739957+ZISARHUB@users.noreply.github.com>
2021-12-20 16:34:21 +0700

    Update IMainHub.lua

--- IMainHub.lua
+++ IMainHub.lua
@@ -1,6 +1,25 @@
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZISARHUB/EEZZ/main/UI", true))()
+
 local Window = Lib:Window("IMainHub")
+
 Window:Button("Mukuro hub", function()
 _G.Color = Color3.fromRGB(0, 255, 255)
 _G.Toggle = Enum.KeyCode.RightControl
 loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua")()
+end)
+
+Window:Button("Islandfsc", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TizCof/islandfsc/main/script.lua"))()
+end)
+
+Window:Button("AstroHub", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AstroScripter/Games/main/Main/BloxFruits/AstroHub-Free.lua"))()
+end)
+
+Window:Button("NightsTimeZ", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NightsTimeZ/Night_Script/main/BloxFruit.lua"))()
+end)
+
+Window:Button("HULKUHUB", function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/HULKUexe/-FreeUScript-/main/3GAME")() 
 end)
