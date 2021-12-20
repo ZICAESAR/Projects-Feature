c680f6ae5042c5c6511c905bd71a4e8a8e9c84f8
ZISARHUB <95739957+ZISARHUB@users.noreply.github.com>
2021-12-20 15:34:42 +0700

    Update IMainHub.lua

--- IMainHub.lua
+++ IMainHub.lua
@@ -1,7 +1,4 @@
-local Lib = loadstring(game:HttpGet("http://www.narwhalhacks.xyz/UI/MinecraftUILib.lua", true))()
-
 local Window = Lib:Window("IMainHub")
-
 Window:Button("Mukuro hub", function()
 _G.Color = Color3.fromRGB(0, 255, 255)
 _G.Toggle = Enum.KeyCode.RightControl
