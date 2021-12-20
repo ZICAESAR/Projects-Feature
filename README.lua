4c5ecb1c40ad7035f21f9129e9ff0259ba424d5a
ZISARHUB <95739957+ZISARHUB@users.noreply.github.com>
2021-12-20 15:29:12 +0700

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,1 +1,9 @@
+local Lib = loadstring(game:HttpGet("http://www.narwhalhacks.xyz/UI/MinecraftUILib.lua", true))()
 
+local Window = Lib:Window("IMainHub")
+
+Window:Button("Mukuro hub", function()
+_G.Color = Color3.fromRGB(0, 255, 255)
+_G.Toggle = Enum.KeyCode.RightControl
+loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua")()
+end)
